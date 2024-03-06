c7ac0f110ce26defb77be71f79234729daf81b09
Abdullah Altun <alttun0@gmail.com>
2024-03-06 16:25:45 +0300

    code

---
+++ game/trex_getData.py
@@ -0,0 +1,71 @@
+import keyboard
+import uuid
+import time
+from PIL import Image
+from mss import mss
+
+
+
+mon = {"top":440, "left":760, "width":250, "height":150}
+sct = mss()
+
+i = 0
+
+def record_screen(record_id, key):
+    global i
+    
+    i += 1
+    print("{}: {}".format(key, i))
+    img = sct.grab(mon)
+    im = Image.frombytes("RGB", img.size, img.rgb)
+    im.save("./kaydet/{}_{}_{}.png".format(key, record_id, i))
+    
+is_exit = False
+
+def exit():
+    global is_exit
+    is_exit = True
+    
+keyboard.add_hotkey("esc", exit)
+
+record_id = uuid.uuid4()
+
+while True:
+    
+    if is_exit: break
+
+    try:
+        if keyboard.is_pressed(keyboard.KEY_UP):
+            record_screen(record_id, "up")
+            time.sleep(0.1)
+        elif keyboard.is_pressed(keyboard.KEY_DOWN):
+            record_screen(record_id, "down")
+            time.sleep(0.1)
+        elif keyboard.is_pressed("right"):
+            record_screen(record_id, "right")
+            time.sleep(0.1)
+    except RuntimeError: continue 
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
